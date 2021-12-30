88a16a7629b687505743413afba9efd6bbc81a4e
dexpiper <dexpiper@yandex.ru>
2021-12-30 12:40:04 +0300

    Changing types

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,236 +1,77 @@
 import unittest
 
-from utils import fields
+from utils import thetypes as t
 from tests.utils import cases
 
 
-def parent_validation(slf, cls_for_test, empty_val=''):
-    f = cls_for_test(required=True, nullable=False)
-    slf.assertFalse(f.valid)
-    slf.assertIn('LookupError', f.val_info())
-    f.value = empty_val
-    slf.assertFalse(f.valid)
-    slf.assertIn('ValueError', f.val_info())
+kwargs = dict(required=False, nullable=True)
 
 
-class TestValresultClass(unittest.TestCase):
+class R:
 
-    def setUp(self):
-        self.v = fields.ValidationResult('Some error')
-        self.str_message = 'Another error message'
-        self.lst_messages = ['Error here', 'Error there']
+    field = t.Field(**kwargs)
+    charfield = t.CharField(**kwargs)
+    argfield = t.ArgumentsField(**kwargs)
+    email = t.EmailField(**kwargs)
+    phone = t.PhoneField(**kwargs)
+    date = t.DateField(**kwargs)
+    birthday = t.BirthDayField(**kwargs)
+    gender = t.GenderField(**kwargs)
+    clientids = t.ClientIDsField(**kwargs)
 
-    def test_bad_validation_result(self):
-        self.assertEqual(self.v.error_message, 'Some error')
-        self.assertEqual(bool(self.v), False)
-
-    def test_good_validation_result(self):
-        v = fields.ValidationResult()
-        self.assertEqual(v.error_message, None)
-        self.assertEqual(bool(v), True)
-
-    def test_validation_message_append_empty(self):
-        v = fields.ValidationResult()
-        v.append(self.str_message)
-        self.assertEqual(v.error_message, 'Another error message')
-        self.assertEqual(v(), 'Another error message')
-
-    def test_validation_message_append_notempty(self):
-        self.v.append(self.str_message)
-        self.assertEqual(self.v.error_message,
-                         'Some error; Another error message')
-
-    def test_validation_message_append_list(self):
-        self.v.append(self.lst_messages)
-        self.assertEqual(self.v.error_message,
-                         'Some error; Error here; Error there')
+    def __init__(self, **kwargs):
+        self.field = kwargs.get('field', None)
+        self.charfield = kwargs.get('charfield', None)
+        self.argfield = kwargs.get('argfield', None)
+        self.email = kwargs.get('email', None)
+        self.phone = kwargs.get('phone', None)
+        self.date = kwargs.get('date', None)
+        self.birthday = kwargs.get('birthday', None)
+        self.gender = kwargs.get('gender', None)
+        self.clientids = kwargs.get('clientids', None)
 
 
-class TestParentField(unittest.TestCase):
+class TestField(unittest.TestCase):
 
-    def test_field_creation(self):
-        f = fields.Field(required=True, nullable=False)
-        f.value = 'Foobar123'
-        self.assertEqual(f.value, 'Foobar123')
-        self.assertEqual(f.valid, True)
-
-    def test_field_creation_null(self):
-        """No value inserted in mandatory field"""
-        f = fields.Field(required=True)
-        self.assertFalse(f.valid)
-        self.assertIsNotNone(f.val_info())
-        self.assertIn('LookupError', f.val_info())
-
-    def test_field_creation_empty(self):
-        """Empty string in non-nullable field"""
-        f = fields.Field(required=False, nullable=False)
-        f.value = ''
-        self.assertFalse(f.valid)
-        self.assertIn('ValueError', f.val_info())
-
-
-class TestCharField(unittest.TestCase):
-
-    def test_charfield_creation(self):
-        f = fields.CharField(required=True, nullable=False)
-        f.value = 'Foobar'
-        self.assertTrue(f.valid)
-        self.assertEqual(f.value, 'Foobar')
-
-    def test_charfield_parent_validation(self):
-        f = fields.CharField(required=True, nullable=False)
-        f.value = 'Foobar'
-        self.assertTrue(f.valid)
-        f.value = None
-        self.assertFalse(f.valid)
-        self.assertIn('LookupError', f.val_info())
-        f.value = ' '
-        self.assertFalse(f.valid)
-        self.assertIn('ValueError', f.val_info())
-
-    def test_charfield_self_validation_fromnum(self):
-        f = fields.CharField(required=True, nullable=False)
-        f.value = 1234567889
-        self.assertTrue(f.valid)
-        self.assertEqual(f.value, '1234567889')
-
-
-class TestArgumentsField(unittest.TestCase):
-
-    @cases(['{"Foobar": 1234}', '{}', '{"Foo": "bar", "1": 2}'])
-    def test_argfield_creation(self, value):
-        f = fields.ArgumentsField(required=True, nullable=True)
-        f.value = value
-        self.assertTrue(f.valid, f'Error with {value}')
-        self.assertIsInstance(f.value, dict)
-
-    def test_argfield_parent_validation(self):
-        parent_validation(self, fields.ArgumentsField, empty_val='{}')
-
-    def test_argfield_self_validation(self):
-        f = fields.ArgumentsField(required=True, nullable=False)
-        f.value = 'Foobar'
-        self.assertFalse(f.valid)
-        self.assertIn('TypeError', f.val_info())
-        f.value = 12345678
-        self.assertFalse(f.valid)
-        self.assertIn('TypeError', f.val_info())
-
-
-class TestEmailField(unittest.TestCase):
-
-    @cases(['foo@bar.com', 'stupnikov@otus.ru', 'eggs@spam.uk',
-            'all@otus.rocks', 'b@egg.org'])
-    def test_email_creation(self, email):
-        f = fields.EmailField(required=True, nullable=False)
-        f.value = email
-        self.assertTrue(f.valid)
-
-    @cases(['foobar.com', 'Foobar', 'Spam@eggs', 12345])
-    def test_bad_email_creation(self, bad_email):
-        f = fields.EmailField(required=True, nullable=False)
-        f.value = bad_email
-        self.assertFalse(f.valid)
-        self.assertIn('ValueError', f.val_info())
-
-    def test_email_parent_validation(self):
-        parent_validation(self, fields.EmailField)
-
-
-class TestPhoneField(unittest.TestCase):
-
-    @cases(['+79121233239', 790111122233, '89998887766'])
-    def test_phone_creation(self, phone):
-        f = fields.PhoneField(required=True, nullable=False)
-        f.value = phone
-        self.assertTrue(f.valid)
-
-    @cases([12345, 'Foobar', '+8nine890123456'])
-    def test_bad_phone(self, bad_phone):
-        f = fields.PhoneField(required=True, nullable=False)
-        f.value = bad_phone
-        self.assertFalse(f.valid)
-        self.assertIn('ValueError', f.val_info())
-
-    def test_phone_parent_validation(self):
-        parent_validation(self, fields.PhoneField)
-
-
-class TestDateField(unittest.TestCase):
-
-    @cases(['27.12.2021', '01.01.1982', '06.11.1904'])
-    def test_date_creation(self, date):
-        f = fields.DateField(required=True, nullable=False)
-        f.value = date
-        self.assertTrue(f.valid, f'Not valid with {date}')
-
-    @cases(['2712.2021', 24011992, '01011982', '06.11.19042'])
-    def test_bad_date_creation(self, bad_date):
-        f = fields.DateField(required=True, nullable=False)
-        f.value = bad_date
-        self.assertFalse(f.valid, f'Considered valid with {bad_date}')
-        self.assertIn('ValueError', f.val_info())
-
-    def test_date_parent_validation(self):
-        parent_validation(self, fields.DateField)
-
-
-class TestBirthDayField(unittest.TestCase):
-
-    @cases(['27.12.2021', '01.01.1982', '06.11.1958'])
-    def test_birthday_creation(self, date):
-        f = fields.BirthDayField(required=True, nullable=False)
-        f.value = date
-        self.assertTrue(f.valid, f'Not valid with {date}')
-
-    @cases(['27.12.1900', '18.09.1867', '01.01.1000'])
-    def test_bad_date_creation(self, bad_date):
-        f = fields.BirthDayField(required=True, nullable=False)
-        f.value = bad_date
-        self.assertFalse(f.valid, f'Considered valid with {bad_date}')
-        self.assertIn('ValueError', f.val_info())
-
-    def test_date_parent_validation(self):
-        parent_validation(self, fields.BirthDayField)
-
-
-class TestGenderField(unittest.TestCase):
-
-    @cases([1, 2, 0, '1', '2', '0'])
-    def test_good_gender_field(self, value):
-        f = fields.GenderField(required=True, nullable=False)
-        f.value = value
-        self.assertTrue(f.valid, f'Not valid with {value}')
-
-    @cases([3, 'male', 42, '13', 'Foobar', -1])
-    def test_bad_gender_creation(self, bad_value):
-        f = fields.GenderField(required=True, nullable=False)
-        f.value = bad_value
-        self.assertFalse(f.valid, f'Considered valid with {bad_value}')
-        self.assertIn('ValueError', f.val_info())
-
-    def test_gender_parent_validation(self):
-        parent_validation(self, fields.GenderField)
-
-
-class TestClientIDsField(unittest.TestCase):
+    def test_basic_field_instantiation(self):
+        r = R(**dict(field='foo'))
+        self.assertEqual(r.field, 'foo')
 
     @cases([
-            [123, 323, 98765],
-            "[1, 2, 42, 12148124124]",
-            "[42]"
-    ])
-    def test_good_id_field(self, value):
-        f = fields.ClientIDsField(required=True, nullable=False)
-        f.value = value
-        self.assertTrue(f.valid, f'Not valid with {value}')
+            'FOObar', 12345, [], ['Foobar123', 'eggs', 'spam'],
+            [1, 2, 3, 54657, 9834, 1274824, 1214214, 0],
+            {}, {'foo': 'bar', 'bar': 'foo'}
+        ])
+    def test_basic_field(self, value):
+        r = R(**dict(field=value))
+        self.assertEqual(r.field, value)
 
-    @cases([42, 'Foobar', '[42, "Forty-two", 9, 8]'])
-    def test_bad_id_field_creation(self, bad_value):
-        f = fields.ClientIDsField(required=True, nullable=False)
-        f.value = bad_value
-        self.assertFalse(f.valid, f'Considered valid with {bad_value}')
-        self.assertIn('ValueError', f.val_info())
+    @cases(['FOObar', '12345', 'Fafaf81819*&%^@!%$'])
+    def test_good_char_field(self, value):
+        r = R(**dict(charfield=value))
+        self.assertEqual(r.charfield, value)
 
-    def test_id_parent_validation(self):
-        parent_validation(self, fields.ClientIDsField, empty_val='[]')
+    @cases([12345, 123456787, [], {'foo': 'bar'}])
+    def test_bad_char_field(self, value):
+        with self.assertRaises(TypeError):
+            R(**dict(charfield=value))
+
+    @cases([{'FOObar': '12345'}, {}, {'foobar': 1234, 'spam': 'eggs'}])
+    def test_good_arg_field(self, value):
+        r = R(**dict(argfield=value))
+        self.assertEqual(r.argfield, value)
+
+    @cases(['Foobar', 123456787, []])
+    def test_bad_arg_field(self, value):
+        with self.assertRaises(TypeError):
+            R(**dict(argfield=value))
+
+    @cases([{'FOObar': '12345'}, {}, {'foobar': 1234, 'spam': 'eggs'}])
+    def test_good_email(self, value):
+        r = R(**dict(email=value))
+        self.assertEqual(r.email, value)
+
+    @cases(['Foobar', 123456787, []])
+    def test_bad_email(self, value):
+        with self.assertRaises(TypeError):
+            R(**dict(email=value))
---
+++ utils/thetypes.py
@@ -0,0 +1,176 @@
+from abc import ABC, abstractmethod
+import datetime
+import re
+
+from api import FEMALE, MALE, UNKNOWN
+
+
+class Validator(ABC):
+
+    def __set_name__(self, owner, name):
+        self.name = name
+        self.private_name = '_' + name
+
+    def __get__(self, obj, objtype=None):
+        return getattr(obj, self.private_name)
+
+    def __set__(self, obj, value):
+        self.validate(value)
+        setattr(obj, self.private_name, value)
+
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+
+class Field(Validator):
+
+    def __init__(self, required=True, nullable=False):
+        self.required = required
+        self.nullable = nullable
+
+    def validate(self, value):
+        cond1 = False if self.required and value is None else True
+        if not cond1:
+            raise LookupError(f'Field {self.name} should be in request')
+        cond2 = (False if not self.nullable and value in ('', [], {})
+                 else True)
+        if not cond2:
+            raise ValueError(f'Field {self.name} cannot be empty')
+
+
+class CharField(Field):
+
+    def validate(self, value):
+        super().validate(value)
+        if not isinstance(value, (str, type(None))):
+            raise TypeError(f'Field {self.name} should be a str')
+
+    def __set__(self, obj, value):
+        self.validate(value)
+        setattr(obj, self.private_name, value)
+
+
+class ArgumentsField(Field):
+
+    def validate(self, value):
+        super().validate(value)
+        if not isinstance(value, (dict, type(None))):
+            raise TypeError(
+                f'Expected {value!r} in {self.name} to be dict-like object'
+            )
+
+
+class EmailField(CharField):
+    regex = r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$'
+
+    def validate(self, value):
+        super().validate(value)
+        if value is None:
+            return
+        if not re.fullmatch(EmailField.regex, value):
+            raise TypeError(
+                f'Bad email in field {self.name} ({value!r} not valid)'
+            )
+
+
+class PhoneField(Field):
+    regex = r'^7\d{10}$'
+
+    def validate(self, value):
+        if isinstance(value, int):
+            value = str(value)
+        super().validate(value)
+        if value is None:
+            return
+        if not re.fullmatch(PhoneField.regex, value):
+            return ValueError(
+                f'Bad phone number in field {self.name} ({value!r} not valid)'
+            )
+
+    def __set__(self, obj, value):
+        self.validate(value)
+        setattr(obj, self.private_name, str(value))
+
+
+class DateField(CharField):
+    format = '%d.%m.%Y'
+
+    def validate(self, value):
+        super().validate(value)
+        if value is None:
+            return
+        try:
+            datetime.strptime(value, DateField.format)
+        except ValueError:
+            raise ValueError(
+                f'Bad date format in field {self.name} ({value!r} not valid)'
+            )
+
+
+class BirthDayField(DateField):
+
+    def validate(self, value):
+        super().validate(value)
+        if value is None:
+            return
+        date = datetime.strptime(value, DateField.format)
+        delta = datetime.today() - date
+        if delta.days > 365*70:
+            raise ValueError(
+                f'Bad date in field {self.name}: '
+                f'more than 70 years since {value!r}'
+            )
+
+
+class GenderField(Field):
+    genders = UNKNOWN, MALE, FEMALE
+
+    def validate(self, value):
+        super().validate(value)
+        if value is None:
+            return
+        if value not in GenderField.genders:
+            raise ValueError(
+                f'Bad value for {self.name} field ({value!r}). '
+                'Should be an integer from {0, 1, 2}'
+            )
+
+
+class ClientIDsField(Field):
+
+    def validate(self, value):
+        super().validate(value)
+        if value is None:
+            return
+        err_message = (f'Bad value for {self.name} field ({value!r}). '
+                       'Should be a list-like object of integers')
+        if not isinstance(value, list):
+            raise ValueError(err_message)
+        if not all(
+            [True if isinstance(el, int) else False
+             for el in value]
+        ):
+            raise ValueError(err_message)
+
+
+class ClientsInterestsRequest():
+    client_ids = ClientIDsField(required=True, nullable=False)
+    date = DateField(required=False, nullable=True)
+
+
+class OnlineScoreRequest():
+    first_name = CharField(required=False, nullable=True)
+    last_name = CharField(required=False, nullable=True)
+    email = EmailField(required=False, nullable=True)
+    phone = PhoneField(required=False, nullable=True)
+    birthday = BirthDayField(required=False, nullable=True)
+    gender = GenderField(required=False, nullable=True)
+
+
+class MethodRequest():
+    account = CharField(required=False, nullable=True)
+    login = CharField(required=True, nullable=True)
+    token = CharField(required=True, nullable=True)
+    arguments = ArgumentsField(required=True, nullable=True)
+    method = CharField(required=True, nullable=False)
