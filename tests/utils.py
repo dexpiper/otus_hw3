00303a11f5f1f88fe6b64526f2e38ff0ca88eb3f
dexpiper <dexpiper@yandex.ru>
2022-01-16 14:20:28 +0300

    Test parametrize

--- tests/utils.py
+++ tests/utils.py
@@ -8,8 +8,10 @@ def cases(cases):
     def decorator(f):
         @functools.wraps(f)
         def wrapper(*args):
+            self = args[0]
             for c in cases:
                 new_args = args + (c if isinstance(c, tuple) else (c,))
-                f(*new_args)
+                with self.subTest(case=c):
+                    f(*new_args)
         return wrapper
     return decorator
