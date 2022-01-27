a9b84f77c90520a4a4f6749b8bc25d55298b1bdc
dexpiper <dexpiper@yandex.ru>
2022-01-27 17:57:18 +0300

    Test structure

--- tests.py
+++ tests.py
@@ -8,6 +8,17 @@ $ python3 tests.py [testfile, optional (default: all tests)] [-v, --verbose]
 import argparse
 import subprocess
 import os
+import fnmatch
+
+
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+
 
 #
 # Running tests
@@ -24,8 +35,9 @@ command = ['python', '-m', 'unittest']
 if args.testfile == 'all':
     command += ['discover', '.', "test_*.py"]
 else:
-    path = f'tests/test_{args.testfile}.py'
+    path = find(f'test_{args.testfile}.py', 'tests')[0]
     assert os.path.isfile(path), f'Test file "{args.testfile}" not found'
+    print(path)
     command.append(path)
 if args.verbose:
     print('Running in verbose mode')
